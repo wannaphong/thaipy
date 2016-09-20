a830ea01ffa31e8ca6c1f8069284a9b985092aa3
Wannaphong Phatthiyaphaibun <wannaphong@yahoo.com>
2016-09-20 19:52:02 +0700

    Update thaipy.py
    เพิ่มเงื่อนไข re ไม่ให้แทนที่ข้อความที่อยู่ในสตริง

--- thaipy.py
+++ thaipy.py
@@ -14,11 +14,11 @@ class ThpyPlugin(object):
     pass
 
 def multiple_replace(dict, text):
-  # Create a regular expression  from the dictionary keys
-  regex = re.compile("(%s)" % "|".join(map(re.escape, dict.keys())))
-
-  # For each match, look-up corresponding value in dictionary
-  return regex.sub(lambda mo: dict[mo.string[mo.start():mo.end()]], text)
+	# Create a regular expression  from the dictionary keys
+	regex = re.compile('((^["]*$)%s(^["]*$))' % "|".join(map(re.escape, dict.keys())))
+	#print(regex)
+	# For each match, look-up corresponding value in dictionary
+	return regex.sub(lambda mo: dict[mo.string[mo.start():mo.end()]], text)
 
 # Simplized thai keywords
 class th_keyword(ThpyPlugin):
