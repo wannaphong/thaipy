6db14ba379427bf2445d3402e43b9c7755c663dc
Wannaphong <wannaphong@yahoo.com>
2016-09-21 20:26:07 +0700

    setup.py - install

---
+++ setup.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+from setuptools import setup
+from setuptools import find_packages
+
+requirements = [
+    # TODO: put package requirements here
+]
+
+setup(
+    name='thaipy',
+    version='0.0.1',
+    description="Write Python 3 language in thai lang.",
+    long_description=readme,# + '\n\n' + history,
+    author="Wannaphong Phatthiyaphaibun",
+    author_email='wannaphong@yahoo.com',
+    url='https://github.com/wannaphongcom/thaipy',
+    packages=find_packages(),
+    package_data={'pythainlp.corpus':['thaipos.json','thaiword.txt']},
+    include_package_data=True,
+    install_requires=requirements,
+    license="Apache Software License 2.0",
+    zip_safe=False,
+    entry_points = """
+    [console_scripts]
+    thaipy = thaipy:commandline
+    """,
+    keywords='thaipy',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Education',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License',
+        'Natural Language :: Thai',
+        'Operating System :: OS Independent',
+        'Topic :: Software Development :: Code Generators'],
+        'Programming Language :: Python :: 3',
+    ],
+    test_suite='tests',
+    tests_require=test_requirements
+)
